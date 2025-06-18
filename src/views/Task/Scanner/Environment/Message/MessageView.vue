166ee36f79330646294814f46aa82990074f478a
GaoZQi <Ga0ZQ1@outlook.com>
2025-06-18 23:30:06 +0800

    fix: 更新启动Agent的API路径以包含任务ID

--- src/views/Task/Scanner/Environment/Message/MessageView.vue
+++ src/views/Task/Scanner/Environment/Message/MessageView.vue
@@ -71,7 +71,7 @@ async function startAgent() {
   try {
     // 1. 启动 agent 模式
     console.log('正在启动 Agent 模式...')
-    await axios.post('http://localhost:8000/agent/start', {
+    await axios.post('http://localhost:8000/agent/' + task.id + '/start', {
       target: config.target,
       goal: config.goal,
       iteration_limit: 20,
